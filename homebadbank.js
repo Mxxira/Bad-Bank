6479f69fee4a607843af64199c4e07c15fd1e1c7
Miranda Christensen <97591732+Mxxira@users.noreply.github.com>
2022-07-19 13:45:03 -0700

    Add files via upload

---
+++ homebadbank.js
@@ -0,0 +1,11 @@
+function Home(){
+  return (
+    <Card
+      txtcolor="blue"
+      header="Bad Bank Landing Module"
+      title="Welcome to the Bank"
+      text="You can move around the bank using the navigation bar."
+      body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+    />    
+  );  
+}
